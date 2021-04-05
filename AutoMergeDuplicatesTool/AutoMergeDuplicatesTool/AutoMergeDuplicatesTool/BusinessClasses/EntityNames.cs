35586ab6ad07e2b6ab765046710fa0a6606d6ff1
João Bastos <bastosj@users.noreply.github.com>
2021-04-05 13:12:06 +0100

    Initial project commit

---
+++ AutoMergeDuplicatesTool/AutoMergeDuplicatesTool/AutoMergeDuplicatesTool/BusinessClasses/EntityNames.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMergeDuplicatesTool
+{
+    /// <summary>
+    /// Class used to populate the combobox with the Names and Logical Names of the entities in CRM
+    /// </summary>
+    public class EntityNames
+    {
+        public string Name { get; set; }
+        public string LogicalName { get; set; }
+    }
+}
